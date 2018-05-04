0ff0b0bea04f0ee1f1c424dd36721c556b45a087
Deepak Vashist <deepaksvashist@gmail.com>
2018-05-04 01:59:15 -0300

    add width search

--- search_methods/backends/width.py
+++ search_methods/backends/width.py
@@ -6,10 +6,34 @@ class Search(SearchAbstract):
     NAME = "Busca em Largura"
 
     def __init__(self, image_array):
+        """
+        Partindo de um vértice inicial, ela explora todos os vértices vizinhos.
+        Em seguida, para cada vértice vizinho, ela repete esse processo,
+        visitando os vértices ainda inexplorados.
+        A Busca em Largura utiliza filas.
+        """
         super(Search, self).__init__(image_array)
+        self.graph_solution = {}
+        self.graph_analysis()
 
     def get_problem_image_solution(self):
         pass
 
     def get_search_response_payload(self):
         pass
+
+    def graph_analysis(self):
+        past_row_positions = None
+
+        for row in self.graph:
+            if not past_row_positions:
+                self.graph_solution[row] = self.graph[row]
+                past_row_positions = self.graph[row]
+                continue
+
+            self.graph_solution[row] = []
+            for i in self.graph[row]:
+                if i in past_row_positions:
+                    self.graph_solution[row].append(i)
+
+            past_row_positions = self.graph_solution[row]
--- search_methods/base.py
+++ search_methods/base.py
@@ -3,8 +3,23 @@ class SearchAbstract:
     def __init__(self, image_array):
         self.image_array = image_array
 
+        self.graph = {}
+        self.set_graph()
+
     def get_problem_image_solution(self):
         return NotImplementedError
 
     def get_search_response_payload(self):
         return NotImplementedError
+
+    def set_graph(self):
+        for row_index, image_row in enumerate(self.image_array):
+            self.graph['pixels_column_{}'.format(row_index)] = []
+            past_parent_index = 0
+
+            for pixel_index, pixel in enumerate(image_row):
+                if not all(pixel == [255, 255, 255, 255]) and \
+                        past_parent_index + 1 != pixel_index:
+                    self.graph['pixels_column_{}'.format(row_index)]. \
+                        append(pixel_index)
+                    past_parent_index = pixel_index
